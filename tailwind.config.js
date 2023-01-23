ee159a5f8f1b69c378e92c140f5cd5cd3acd9e49
Margub Murshed <83542971+margubmurshed@users.noreply.github.com>
2023-01-23 23:53:18 +0600

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-01": "#1c1c1c",
+      },
+    },
+  },
+  plugins: [],
+};
